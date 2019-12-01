483314360f2afaa43c30fe971d55c2a9c531a12d
Light2288 <davide.aliti@gmail.com>
2019-12-01 16:43:36 +0100

    created theme

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
 import logo from '../img/logo.svg'
 
 const Navbar = class extends React.Component {
@@ -83,9 +82,6 @@ const Navbar = class extends React.Component {
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
               </a>
             </div>
           </div>
