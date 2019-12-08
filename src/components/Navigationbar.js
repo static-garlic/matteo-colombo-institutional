a6a945fe1cdfdaf800fce3eb3e1bab3f7f43c9bc
Light2288 <davide.aliti@gmail.com>
2019-12-08 17:20:53 +0100

    refactored navbar with reactstrap

--- src/components/Navigationbar.js
+++ src/components/Navigationbar.js
@@ -1,66 +1,64 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Link} from 'gatsby'
-import {Navbar} from "reactstrap";
+import {Button, Collapse, Nav, Navbar, NavbarToggler, NavItem} from "reactstrap";
 
-const NavigationBar = class extends React.Component {
+const NavigationBar = () => {
+    const [isOpen, setIsOpen] = useState(false);
 
-    render() {
-        return (
-            <Navbar color="white" light className="navbar-expand-md">
-                <Link to="/" className="navbar-item" title="Logo">
-                    <button className="btn btn-primary">LOGO</button>
+    const toggle = () => setIsOpen(!isOpen);
+
+    return (
+        <Navbar color="white" light className="navbar-expand-md">
+                <Link to="/" className="navbar-brand" title="Logo">
+                    <Button color="primary">LOGO</Button>
                 </Link>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item text-primary">
-                            <Link className="nav-link" to="/biography" activeClassName="active">
-                                Biography
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/research">
-                                Research
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/publications">
-                                Publications
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/infographics-and-maps">
-                                Infographics and maps
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/teaching">
-                                Teaching
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/language-skills">
-                                Language Skills
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/technical-skills">
-                                Technical Skills
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/media">
-                                Media
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
-            </Navbar>
-        )
-    }
-}
+            <NavbarToggler onClick={toggle}/>
+            <Collapse isOpen={isOpen} navbar>
+                <Nav className="mr-auto" navbar>
+                    <NavItem>
+                        <Link className="nav-link" to="/biography" activeClassName="text-primary">
+                            Biography
+                        </Link>
+                    </NavItem>
+                    <NavItem>
+                        <Link className="nav-link" to="/research" activeClassName="text-primary">
+                            Research
+                        </Link>
+                    </NavItem>
+                    <NavItem>
+                        <Link className="nav-link" to="/publications" activeClassName="text-primary">
+                            Publications
+                        </Link>
+                    </NavItem>
+                    <NavItem>
+                        <Link className="nav-link" to="/infographics-and-maps" activeClassName="text-primary">
+                            Infographics and maps
+                        </Link>
+                    </NavItem>
+                    <NavItem>
+                        <Link className="nav-link" to="/teaching" activeClassName="text-primary">
+                            Teaching
+                        </Link>
+                    </NavItem>
+                    <NavItem>
+                        <Link className="nav-link" to="/language-skills" activeClassName="text-primary">
+                            Language Skills
+                        </Link>
+                    </NavItem>
+                    <NavItem>
+                        <Link className="nav-link" to="/technical-skills" activeClassName="text-primary">
+                            Technical Skills
+                        </Link>
+                    </NavItem>
+                    <NavItem>
+                        <Link className="nav-link" to="/media" activeClassName="text-primary">
+                            Media
+                        </Link>
+                    </NavItem>
+                </Nav>
+            </Collapse>
+        </Navbar>
+    )
+};
 
-export default NavigationBar
+export default NavigationBar;
