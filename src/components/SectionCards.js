e4e4ce6eb862eee465179840eea2fb0302166853
Light2288 <davide.aliti@gmail.com>
2020-01-12 23:45:48 +0100

    changed title inside jumbotron created section cards

---
+++ src/components/SectionCards.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import {Button, Card, CardBody, CardText, CardTitle, Col, Row} from "reactstrap";
+import PropTypes from "prop-types";
+import PreviewCompatibleImage from "./PreviewCompatibleImage";
+
+export const SectionCards = ({sectionCards}) => (
+    <div className="SectionCards">
+      <Row>
+        {
+          sectionCards.map(sectionCard =>
+              <Col sm={Math.floor(12/sectionCards.length)} key={sectionCard.sectionTitle}>
+                <Card>
+                  <PreviewCompatibleImage imageInfo={sectionCard} />
+                  <CardBody className="text-center">
+                    <CardTitle className="text-primary">{sectionCard.sectionTitle}</CardTitle>
+                    <CardText>{sectionCard.sectionDesc}</CardText>
+                    <Button color="primary">Show more</Button>
+                  </CardBody>
+                </Card>
+              </Col>
+          )
+        }
+      </Row>
+    </div>
+)
+
+SectionCards.propTypes = {
+  sectionCards: PropTypes.arrayOf(
+      PropTypes.shape({
+        image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+        sectionTitle: PropTypes.string,
+        sectionDesc: PropTypes.string,
+      })
+  ),
+}
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,156 +1,67 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {Link, graphql} from 'gatsby'
+import {graphql} from 'gatsby'
 
 import Layout from '../components/Layout'
-import Features from '../components/Features'
-import BlogRoll from '../components/BlogRoll'
-import {Jumbotron} from "reactstrap";
+import {Col, Container, Jumbotron, Row} from "reactstrap";
+import {SectionCards} from "../components/SectionCards";
 
 export const IndexPageTemplate = ({
-                                      image,
-                                      title,
-                                      heading,
-                                      subheading,
-                                      mainpitch,
-                                      description,
-                                      intro,
+                                    image,
+                                    title,
+                                    sectionCards,
                                   }) => (
-    <div>
-        <Jumbotron
-            fluid style={{
-            background: `url(${
-                !!image.childImageSharp ? image.childImageSharp.fluid.src : image
-                }) no-repeat center center`,
-            height: "30rem",
-            "background-size": "cover"
-        }}
-        >
-            <div
-                style={{
-                    display: 'flex',
-                    height: '150px',
-                    lineHeight: '1',
-                    justifyContent: 'space-around',
-                    alignItems: 'left',
-                    flexDirection: 'column',
-                }}
-            >
-                <h1
-                    className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
-                    style={{
-                        boxShadow:
-                            'rgb(82, 177, 234) 0.5rem 0px 0px, rgb(82, 177, 234) -0.5rem 0px 0px',
-                        backgroundColor: 'rgb(82, 177, 234)',
-                        color: 'white',
-                        lineHeight: '1',
-                        padding: '0.25em',
-                    }}
-                >
-                    {title}
-                </h1>
-                <h3
-                    className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
-                    style={{
-                        boxShadow:
-                            'rgb(82, 177, 234) 0.5rem 0px 0px, rgb(82, 177, 234) -0.5rem 0px 0px',
-                        backgroundColor: 'rgb(82, 177, 234)',
-                        color: 'white',
-                        lineHeight: '1',
-                        padding: '0.25em',
-                    }}
-                >
-                    {subheading}
-                </h3>
-            </div>
-        </Jumbotron>
-        <section className="section section--gradient">
-            <div className="container">
-                <div className="section">
-                    <div className="columns">
-                        <div className="column is-10 is-offset-1">
-                            <div className="content">
-                                <div className="content">
-                                    <div className="tile">
-                                        <h1 className="title">{mainpitch.title}</h1>
-                                    </div>
-                                    <div className="tile">
-                                        <h3 className="subtitle">{mainpitch.description}</h3>
-                                    </div>
-                                </div>
-                                <div className="columns">
-                                    <div className="column is-12">
-                                        <h3 className="has-text-weight-semibold is-size-2">
-                                            {heading}
-                                        </h3>
-                                        <p>{description}</p>
-                                    </div>
-                                </div>
-                                <Features gridItems={intro.blurbs}/>
-                                <div className="columns">
-                                    <div className="column is-12 has-text-centered">
-                                        <Link className="btn" to="/products">
-                                            See all products
-                                        </Link>
-                                    </div>
-                                </div>
-                                <div className="column is-12">
-                                    <h3 className="has-text-weight-semibold is-size-2">
-                                        Latest stories
-                                    </h3>
-                                    <BlogRoll/>
-                                    <div className="column is-12 has-text-centered">
-                                        <Link className="btn" to="/blog">
-                                            Read more
-                                        </Link>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
+    <div className="IndexPage">
+      <Jumbotron
+          fluid style={{
+        background: `url(${
+            !!image.childImageSharp ? image.childImageSharp.fluid.src : image
+            }) no-repeat center center`,
+        height: "30rem",
+        "background-size": "cover"
+      }}
+      >
+        <Row>
+          <Col sm={{size: 6, offset: 1}}>
+            <h1 className="py-2 text-primary">
+              {title}
+            </h1>
+          </Col>
+        </Row>
+      </Jumbotron>
+      <Container fluid className="px-lg-5">
+        <SectionCards sectionCards={sectionCards}/>
+      </Container>
     </div>
-)
+);
 
 IndexPageTemplate.propTypes = {
-    image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-    title: PropTypes.string,
-    heading: PropTypes.string,
-    subheading: PropTypes.string,
-    mainpitch: PropTypes.object,
-    description: PropTypes.string,
-    intro: PropTypes.shape({
-        blurbs: PropTypes.array,
-    }),
-}
+  image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+  title: PropTypes.string,
+  sectionCards: PropTypes.array,
+};
 
 const IndexPage = ({data}) => {
-    const {frontmatter} = data.markdownRemark
+  const {frontmatter} = data.markdownRemark;
 
-    return (
-        <Layout>
-            <IndexPageTemplate
-                image={frontmatter.image}
-                title={frontmatter.title}
-                heading={frontmatter.heading}
-                subheading={frontmatter.subheading}
-                mainpitch={frontmatter.mainpitch}
-                description={frontmatter.description}
-                intro={frontmatter.intro}
-            />
-        </Layout>
-    )
-}
+  return (
+      <Layout>
+        <IndexPageTemplate
+            image={frontmatter.image}
+            title={frontmatter.title}
+            sectionCards={frontmatter.sectionCards}
+        />
+      </Layout>
+  )
+};
 
 IndexPage.propTypes = {
-    data: PropTypes.shape({
-        markdownRemark: PropTypes.shape({
-            frontmatter: PropTypes.object,
-        }),
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.shape({
+      frontmatter: PropTypes.object,
     }),
-}
+  }),
+};
 
 export default IndexPage
 
@@ -166,28 +77,18 @@ export const pageQuery = graphql`
             }
           }
         }
-        heading
-        subheading
-        mainpitch {
-          title
-          description
-        }
-        description
-        intro {
-          blurbs {
-            image {
-              childImageSharp {
-                fluid(maxWidth: 240, quality: 64) {
-                  ...GatsbyImageSharpFluid
-                }
+        sectionCards {
+          image {
+            childImageSharp {
+              fluid(maxWidth: 240, quality: 64) {
+                ...GatsbyImageSharpFluid
               }
             }
-            text
           }
-          heading
-          description
+          sectionTitle
+          sectionDesc
         }
       }
     }
   }
-`
+`;
