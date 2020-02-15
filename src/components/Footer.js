3e3f37a2a7f5de99edbc5a7279832bc76cd2db6c
Light2288 <davide.aliti@gmail.com>
2020-02-15 12:05:55 +0100

    added Footer.js fixed scss variables

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,101 @@
 import React from 'react'
-import { Link } from 'gatsby'
-
-import logo from '../img/logo.svg'
-import facebook from '../img/social/facebook.svg'
-import instagram from '../img/social/instagram.svg'
-import twitter from '../img/social/twitter.svg'
-import vimeo from '../img/social/vimeo.svg'
+import {Col, Container, Row} from "reactstrap";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import {Link} from "gatsby";
 
 const Footer = class extends React.Component {
   render() {
     return (
       <div className="footer">
-        <div className="container">
-          <span className="text-muted">Place sticky footer content here.</span>
-        </div>
+        <Container fluid>
+          <Row>
+            <Col sm="6" className="text-center">
+              <Row>
+                <Col>
+                  <h5 className="text-white pt-3 pb-2">Links</h5>
+                </Col>
+              </Row>
+              <Row className="pb-2">
+                <Col sm="4">
+                  <Link className="nav-link text-white p-0" to="/">
+                    Home
+                  </Link>
+                </Col>
+                <Col sm="4">
+                  <Link className="nav-link text-white p-0" to="/biography">
+                    Biography
+                  </Link>
+                </Col>
+                <Col sm="4">
+                  <Link className="nav-link text-white p-0" to="/research">
+                    Research
+                  </Link>
+                </Col>
+              </Row>
+              <Row className="pb-2">
+                <Col sm="4">
+                  <Link className="nav-link text-white p-0" to="/publications">
+                    Publications
+                  </Link>
+                </Col>
+                <Col sm="4">
+                  <Link className="nav-link text-white p-0" to="/infographics-and-maps">
+                    Infographics and maps
+                  </Link>
+                </Col>
+                <Col sm="4">
+                  <Link className="nav-link text-white p-0" to="/teaching">
+                    Teaching
+                  </Link>
+                </Col>
+              </Row>
+              <Row className="pb-2">
+                <Col sm="4">
+                  <Link className="nav-link text-white p-0" to="/language-skills">
+                    Language Skills
+                  </Link>
+                </Col>
+                <Col sm="4">
+                  <Link className="nav-link text-white p-0" to="/technical-skills">
+                    Technical Skills
+                  </Link>
+                </Col>
+                <Col sm="4">
+                  <Link className="nav-link text-white p-0" to="/media">
+                    Media
+                  </Link>
+                </Col>
+              </Row>
+            </Col>
+            <Col sm="6" className="text-white text-center">
+              <Row>
+                <Col>
+                  <h5 className="pt-3 pb-2">Contacts and Social Media</h5>
+                </Col>
+              </Row>
+              <Row className="pb-2">
+                <Col>
+                  <FontAwesomeIcon icon={faEnvelope}/>{` Mail: `}
+                  <a className="text-white" href="mailto:matteo.colombo@infodemia.it">matteo.colombo@infodemia.it</a>
+                </Col>
+              </Row>
+              <Row className="pb-2">
+                <Col>
+                  <FontAwesomeIcon icon={faTwitter}/>{` Twitter: `}
+                  <a className="text-white" href="https://twitter.com/philoteo" target="_blank" rel="noopener noreferrer">@philoteo</a>
+                </Col>
+              </Row>
+              <Row className="pb-2">
+                <Col>
+                  <FontAwesomeIcon icon={faLinkedin}/>{` LinkedIn: `}
+                  <a className="text-white" href="https://www.linkedin.com/in/matteo-colombo-21238130/" target="_blank" rel="noopener noreferrer">matteo-colombo</a>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
       </div>
     )
   }
