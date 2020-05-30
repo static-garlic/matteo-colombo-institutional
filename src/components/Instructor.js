7969b24d9a874f5542239d2d80180253b4208454
Light2288 <davide.aliti@gmail.com>
2020-05-30 18:43:45 +0200

    test

--- src/components/Instructor.js
+++ src/components/Instructor.js
@@ -15,7 +15,7 @@ export const Instructor = ({instructor}) =>
               {
                 lecture.studentsEvaluationsFile ? (
                   <p>{`Download students evaluations `}
-                    <a  target="_blank" rel="noopener noreferrer" href={`${lecture.studentsEvaluationsFile.substring(lecture.studentsEvaluationsFile.indexOf("/static") + 1)}`}>here</a>
+                    <a  target="_blank" rel="noopener noreferrer" href={`${lecture.studentsEvaluationsFile.substring(lecture.studentsEvaluationsFile.indexOf("/static") + 1)}`}>here {lecture.studentsEvaluationsFile}</a>
                   </p>
                 ) : null
               }
