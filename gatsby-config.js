206e2c6550616ecd6f59be540e9145b89e874e3b
Light2288 <davide.aliti@gmail.com>
2026-09-10 01:03:04 +0200

    Remove obsolete Sass loader option

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,9 +8,6 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     {
       resolve: 'gatsby-plugin-sass',
-      options: {
-        indentedSyntax: true
-      },
     },
     {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
